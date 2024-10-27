06de8c2f94bb94294f681982eb3d1c0bf61fd1f8
nullcharge <nullagent07@gmail.com>
2024-10-27 18:46:08 +0100

    update : user model

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
     is_subscription_active = Column(Boolean, default=False)
-
