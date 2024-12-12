939428c8f9347b7a2925f6f79f3f6cdbb42e50fa
nullcharge <nullagent07@gmail.com>
2024-12-12 17:16:15 +0100

    add : capabilities and name in assistant profile model

--- app/domain/models/assistant_profiles.py
+++ app/domain/models/assistant_profiles.py
@@ -1,6 +1,7 @@
 import uuid
+from typing import List
 
-from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy import ForeignKey, String, Text, ARRAY
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.domain.models.base import Base
@@ -12,7 +13,9 @@ class AssistantProfiles(Base):
 
     id: Mapped[str] = mapped_column(String(255), primary_key=True)
     creator_user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    name: Mapped[str] = mapped_column(String(255))
     instruction: Mapped[str] = mapped_column(Text)
+    capabilities: Mapped[List[str]] = mapped_column(ARRAY(String(255)))
 
     # Отношения
     creator: Mapped[Users] = relationship(back_populates="assistants")
